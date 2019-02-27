a4303c8c5ba21880dd943da8f07c737f6f1323f9
Mathilde <baquet.mathilde@gmail.com>
2019-02-27 16:57:35 +0100

    modeling title animation

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,4 +9,23 @@ window.onload = () => {
         txt = "Bonjour " + person + "! Comment ca va?";
     }
     document.getElementById("popUp").innerText = txt;
-}
+
+    // animation titre
+        anime.timeline({loop: true})
+        .add({
+            targets: '.ml15 .word',
+            scale: [14,1],
+            opacity: [0,1],
+            easing: "easeOutCirc",
+            duration: 800,
+            delay: function(el, i) {
+              return 800 * i;
+            }
+        }).add({
+            targets: '.ml15',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+} 
